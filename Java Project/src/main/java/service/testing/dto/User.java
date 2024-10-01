fe58630c20005b2ed2280414e4ba77b3e22fad00
Vladimir-Lutic <luticvladimircalc@gmail.com>
2024-10-01 10:52:45 +0300

    Today's tasks added

---
+++ Java Project/src/main/java/service/testing/dto/User.java
@@ -0,0 +1,27 @@
+package service.testing.dto;
+
+public class User {
+
+    private String login = "";
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public static final String LOGIN = "Vladimir-Lutic";
+    public static final int ID = 183031966;
+
+}
---
+++ Java Project/src/test/java/CreateRepo.java
@@ -0,0 +1,69 @@
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import service.testing.PropertyReader;
+
+import java.io.IOException;
+
+public class CreateRepo {
+
+    CloseableHttpClient client;
+    CloseableHttpResponse response;
+
+    @BeforeMethod
+    public void setup(){
+
+        client = HttpClientBuilder.create().build();
+    }
+
+    @AfterMethod
+    public void closeResource() throws IOException {
+        client.close();
+        response.close();
+    }
+
+    @Test(priority = 1)
+    public void createRepo() throws Exception{
+
+        HttpPost request = new HttpPost(PropertyReader.getProperty("base_url") + "/user/repos");
+        request.setHeader(HttpHeaders.AUTHORIZATION, "token " + PropertyReader.getProperty("token"));
+
+        String json = "{\"name\": \"hello-world2\"}";
+        request.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
+
+        System.out.println(json);
+
+        response = client.execute(request);
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        Assert.assertEquals(statusCode, 201);
+    }
+
+    @Test(priority = 2)
+    public void deleteRepo() throws Exception{
+
+        HttpDelete request = new HttpDelete("https://api.github.com/repos/Vladimir-Lutic/hello-world2");
+        request.setHeader(HttpHeaders.AUTHORIZATION, "token " + PropertyReader.getProperty("token"));
+
+        response = client.execute(request);
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        Assert.assertEquals(statusCode, 204);
+
+
+        response = client.execute(request);
+        statusCode = response.getStatusLine().getStatusCode();
+        Assert.assertEquals(statusCode, 404);
+
+    }
+
+}
